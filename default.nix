04068b718bd778ff6cc7c509741d4c1e16038238
Szczyp <qb@szczyp.com>
2016-06-17 20:21:11 +0200

    ghc 8.0.1

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 with (import <nixpkgs> {});
-with haskell.packages.ghc7103;
+with haskell.packages.ghc801;
 (mkDerivation {
   pname = "twitch";
   version = "0.1.0.0";
   src = ./.;
   buildDepends = [ aeson base boxes classy-prelude lens lens-aeson wreq];
-  buildTools = [ cabal-install ghc-mod stylish-haskell hoogle hasktags hlint ];
+  buildTools = [ cabal-install ghc-mod stylish-haskell hoogle hlint ];
   license = stdenv.lib.licenses.gpl3;
 }).env
