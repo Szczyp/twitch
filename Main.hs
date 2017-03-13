11f9123a155bf17902239db5a08c0b4927f73324
Szczyp <qb@szczyp.com>
2017-03-13 22:32:52 +0100

    Fast 'n sloppy

--- Main.hs
+++ Main.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE DeriveGeneric, GADTs, NamedFieldPuns, NoImplicitPrelude,
-             NoMonomorphismRestriction, OverloadedStrings #-}
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric, GADTs, NamedFieldPuns,
+             NoImplicitPrelude, OverloadedStrings #-}
 
 module Main where
 
 import ClassyPrelude
+import Control.Category
 import Control.Lens
 import Data.Aeson
 import Data.Aeson.Lens
+import Data.Either.Utils
 import Data.List              (transpose)
 import Data.Time
 import Network.Wreq
@@ -17,11 +19,8 @@ import Text.PrettyPrint.Boxes
 data Config = Config { apiRoot  :: Text
                      , clientId :: Text
                      , channels :: [Text]
-                     } deriving (Generic)
+                     } deriving (Generic, FromJSON)
 
-instance FromJSON Config
-
-getStreams :: Config -> IO [(Text, Text, Text, Text, Text, Text)]
 getStreams Config {clientId, apiRoot, channels} = do
   let opts = defaults & header "Client-ID" .~ [encodeUtf8 clientId]
   r <- getWith opts . unpack $ apiRoot ++ query
@@ -36,36 +35,32 @@ getStreams Config {clientId, apiRoot, channels} = do
   where query = "?stream_type=live&limit=100&channel=" ++ intercalate "," channels
         toText = to $ pack . show
 
-duration :: UTCTime -> Text -> Text
 duration base time = fromMaybe "" $ do
   t <- parse . unpack $ time
   return . pack . format . quotRem (truncate $ diffUTCTime base t / 60) $ 60
   where parse = parseTimeM False defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ"
         format (h, m) = show h ++ "h" ++ " " ++ show m ++ "m"
 
-printInfo :: [(Text, Text, Text, Text, Text)] -> IO ()
-printInfo =
-  printBox
-  . hsep 2 left
-  . map (vcat left)
-  . transpose
-  . map (^.. each . to (text . unpack))
-  . (("CHANNEL:", "DURATION:", "VIEWERS:", "VIDEO:", "STATUS:") :)
+printInfo streams = do
+  now <- getCurrentTime
+  streams
+    & each . _2 %~ duration now
+    & each . _6 %~ trimStatus
+    & each %~ (\(n, c, v, height, fps, s) -> (n, c, v, height ++ "@" ++ fps, s))
+    & (("CHANNEL:", "DURATION:", "VIEWERS:", "VIDEO:", "STATUS:") :)
+    & map (^.. each . to (text . unpack))
+    & transpose
+    & map (vcat left)
+    & hsep 2 left
+    & printBox
 
-trimStatus :: (IsSequence t, IsString t) => t -> t
 trimStatus s = let s' = take 80 s in if length s' > 80 then s' ++ "..." else s
 
 main :: IO ()
 main = do
   mapM_ (`hSetEncoding` utf8) [stdout, stderr]
-  dir <- getAppUserDataDirectory "twitch"
-  configJson <- readFile (dir </> "twitch.config")
-  case decodeStrict configJson of
-    Nothing -> putStrLn "Can't parse config"
-    Just config -> do
-      streams <- getStreams config
-      now <- getCurrentTime
-      printInfo $ streams
-        & each . _2 %~ duration now
-        & each . _6 %~ trimStatus
-        & each %~ \(n, c, v, height, fps, s) -> (n, c, v, height ++ "@" ++ fps, s)
+  getAppUserDataDirectory "twitch"
+    <&> (</> "twitch.config")
+    >>= readFile
+    >>= (eitherDecodeStrict >>> forceEither >>> getStreams)
+    >>= printInfo
