b2a8f487ca6e28559a0ba6aa8bf59659fe6240c3
Szczyp <qb@szczyp.com>
2020-01-29 15:46:18 +0100

    Improve default.nix and shell.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,32 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-haskellPackages.callCabal2nix "twitch" (nix-gitignore.gitignoreSource [] ./.) {}
+let
+  name = "twitch";
+
+  src = nix-gitignore.gitignoreSource [] ./.;
+
+  haskellPackages = haskell.packages.ghc865.override {
+    overrides = self: super: {
+      ${name} = self.callCabal2nix name src {};
+    };
+  };
+
+  drv = haskell.lib.justStaticExecutables haskellPackages.${name};
+
+  shell = haskellPackages.shellFor {
+    withHoogle = true;
+    packages = p: [ p.${name} ];
+    buildInputs = with haskellPackages; [
+      cabal-install
+      apply-refact
+      hindent
+      hlint
+      stylish-haskell
+      hasktags
+      hoogle
+      cabal2nix
+      (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
+    ];
+  };
+in
+drv // { inherit shell; }
--- shell.nix
+++ shell.nix
@@ -1,14 +1,1 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs.haskellPackages;
-(callPackage ./. {}).env.overrideAttrs(old: {
-  buildInputs = old.buildInputs ++ [
-    cabal-install
-    cabal2nix
-    apply-refact
-    hindent
-    hlint
-    stylish-haskell
-    hasktags
-    hoogle
-  ];
-})
+(import ./. {}).shell
